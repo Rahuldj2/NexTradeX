d4e8b1d9bc6c766114d13bdec3a1b5ba730c9e4e
Rahuldj <rj712@snu.edu.in>
2024-02-04 06:34:11 +0530

    myBids without metamask func call

--- src/pages/myBids.js
+++ src/pages/myBids.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import { getDummyData } from './api/dummydata';
 import ListView from '../components/ListView';
 import  Navbar  from '../components/Navbar';
@@ -7,6 +7,7 @@ import { useMoralis } from "react-moralis";
 // import { useEffect, useState } from "react";
 import { useWeb3Contract } from "react-moralis";
 import { contractABI, contract_address } from "../../contracts/NewContractDetails.js";
+import { m } from 'framer-motion';
 
 
 
@@ -17,20 +18,42 @@ const MyBids = () => {
   const[myBids,SetBids]=useState()
   const [uniqueUserProperties, setUniqueUserProperties] = useState([]);
   useEffect(() => {
+    console.log("ACCESS USING myBids state variable")
+    console.log(myBids)
+  },[myBids]);
+
+
+
+  useEffect(() => {
     console.log("BHAIYYA YE HAI FINAL LELO PLS")
     console.log(uniqueUserProperties)
+
+    const finalMyBids=[]
     for (let i=0;i<uniqueUserProperties.length;i++)
     {
       console.log("namaste")
-      console.log(uniqueUserProperties[i][0].toLowerCase()===account.toLowerCase())
+      if(uniqueUserProperties[i][0].toLowerCase()===account.toLowerCase())
+      {
+        finalMyBids.push(uniqueUserProperties[i])
+      }
     }
-
+    SetBids(finalMyBids)
     //THIS IS THE FINAL OUTPUT
   }, [uniqueUserProperties]);
 
+  const { runContractFunction: getAllBids } = useWeb3Contract({
+    abi: contractABI,
+    contractAddress: contract_address,
+    functionName: "getAllBids",
+    params: { "owner": account,"propertyId":0 }
+  })
+
   useEffect(() => {
+
+    
     const fetchData = async () => {
       try {
+        // await getAllBids();
         const response = await fetch('/api/swagger/fetchBids');
         if (!response.ok) {
           throw new Error('Failed to fetch data');
